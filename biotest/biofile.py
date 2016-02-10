19662d36f06f78ada1fc1d147ad87e954fd37197
Tyghe Vallard <vallardt@gmail.com>
2016-02-10 16:06:08 -0500

    i think this is correct syntax for mocking local open context

--- biotest/biofile.py
+++ biotest/biofile.py
@@ -5,9 +5,7 @@ class MockableFile(mock.MagicMock):
     Provide easily mockable object for the open call
 
     >>> import mock
-    >>> print globals()['__builtins__']
-
-    >>> with mock.patch.object(__builtins__, 'open', MockableFile) as mock_open:
+    >>> with mock.patch('__builtin__.open', MockableFile) as mock_open:
     ...    mock_open.set_contents('foo')
     ...    print open
     ...    x = open('foo.txt').read()

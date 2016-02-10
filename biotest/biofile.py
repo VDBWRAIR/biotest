8158d9e002178a7f702a159e408556aaea7ddf0e
Tyghe Vallard <vallardt@gmail.com>
2016-02-10 15:28:25 -0500

    some example usage

--- biotest/biofile.py
+++ biotest/biofile.py
@@ -3,4 +3,24 @@ import mock
 class MockableFile(mock.MagicMock):
     '''
     Provide easily mockable object for the open call
+
+    import mock
+    from biotest import MockableFile
+
+    with mock.patch(open, MockableFile) as mock_open:
+        mock_open.set_contents('foo')
+        x = open('foo.txt').read()
+        assert x == 'foo'
+
+    with mock.patch(open, MockableFile('foo') as mock_open:
+        x = open('foo.txt').read()
+        assert x == 'foo'
+
+    with mock.patch(open, MockableFile) as mock_open:
+        mock_open.set_contents('foo.txt', 'foo')
+        mock_open.set_contents('bar.txt', 'bar')
+        x = open('foo.txt').read()
+        assert x == 'foo'
+        x = open('bar.txt').read()
+        assert x == 'bar'
     '''

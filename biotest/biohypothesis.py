257a0056f6de0f1610071830f6288161bcd20b02
averagehat <michael.panciera.work@gmail.com>
2016-02-19 15:45:21 -0500

    add general header parser->schema basics

--- biotest/biohypothesis.py
+++ biotest/biohypothesis.py
@@ -3,15 +3,18 @@ from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 import operator
 from fn.iters import accumulate
+from toolz.dicttoolz import update_in
 from toolz.itertoolz import partition
 import string
 from functools import partial
-from itertools import * 
+from itertools import *
 from hypothesis import strategies as st
 from hypothesis import given, assume
 from functools import wraps
 from toolz import compose
 from fn import Stream
+from pyparsing import Literal,  Word, \
+   alphas, nums, quotedString, delimitedList
 
 ##############
 # Hypothesis #
@@ -85,7 +88,7 @@ def interleaved_strategy_factory():
     strategy = st.uuids().map(str).flatmap(
         lambda id:
             st.tuples(
-                seq_rec_strategy_factory(5, 20, idstrat=st.shared(st.just(id), key=id)), 
+                seq_rec_strategy_factory(5, 20, idstrat=st.shared(st.just(id), key=id)),
                 seq_rec_strategy_factory(5, 20, idstrat=st.shared(st.just(id), key=id))))
     return strategy
 
@@ -107,12 +110,15 @@ def ref_with_vcf_dicts_strategy_factory(draw):
     '''
     seq = draw(st.text(alphabet='ACGT', min_size=10, max_size=20))
     size = len(seq)
-    #_not = lambda f: lambda x: not f(x)
     ranges = draw(rolling_sum(1, 3, size/2).map(lambda xs: ifilter(lambda x: x < size, xs)) )#.filter(_not(bool)))
+    # Stream lets you re-use a generator without draining it.
     pairs = Stream() << partition(2, ranges)
     POSs = Stream() << imap(operator.itemgetter(0), pairs)
+    # VCF files start at index 1; python starts at 0
     pairs_offset_1 = imap(lambda x: (x[0] - 1, x[1] - 1), pairs)
-    chunks = map(lambda x: seq[x[0]:x[1]], pairs_offset_1) 
+    #grab the pieces of the reference
+    chunks = map(lambda x: seq[x[0]:x[1]], pairs_offset_1)
+    #random chromosome name
     chrom = draw(st.text(string.ascii_letters))
     vcfs = map(compose(draw, partial(vcf_dict_strategy_factory, chrom)), POSs, chunks)
     #TODO: ranges must be non-empty. Assuming vcfs for now.
@@ -123,16 +129,46 @@ def ref_with_vcf_dicts_strategy_factory(draw):
 def vcf_dict_strategy_factory(draw, chrom, pos, ref):
     '''a generator that returns a single
     VCF dict at a certain position or w/e for testing `call_base`'''
-    alt = draw(st.text(alphabet='ACGT', min_size=0, max_size=6))
+    alts = draw(st.lists(st.text(alphabet='ACGT', min_size=0, max_size=6)), min_size=1, max_size=3)
     #an AO (alternate base count) of 0 doesn't make sense
     ao = draw(st.integers(min_value=1))
     dp = ao + draw(st.integers(min_value=1))
     fields = ['alt', 'ref', 'pos', 'chrom', 'DP', 'AO']
-    values = [alt, ref, pos, chrom, dp, ao]
+    values = [alts, ref, pos, chrom, dp, ao]
     if None in values:
         raise ValueError("Found none value, did the mapping fail?")
     return dict(zip(fields, values))
 
+#TODO: now consensus should throw error if AO > DP
+def parse_header(lines):
+    header =  takwhile(lambda x: x[0] == '#', lines)
+    isMedata = lambda x: x.startswith("##INFO") or x.startswith("##FORMAT")
+    metadata = ifilter(isMedata, header)
+    result = imap(parse_header_line, metadata)
+    return result
+
+#TODO: A way to add arbitrary contraints (< 1), and contraints based on relations (AO < DP). use `assume` ?
+def parse_header_line(lineString):
+    lineName = Literal("##").suppress() + (Literal("FORMAT") | Literal("INFO"))
+    sentence = quotedString(r'"').setParseAction(removeQuotes)
+    def make_kv(key, valParser):
+        return Literal(key) + Literal("=").suppress() + valParser
+    keyVal = make_kv("ID", Word(alphas + '.')) | make_kv("Type", (Literal("Float") | Literal("String") | Literal("Integer"))) | make_kv("Description", sentence) | make_kv("Number",  Word(alphas + nums))
+    fields = delimitedList(keyVal, ",")
+    line = lineName + Literal("=<").suppress() + fields + Literal(">").suppress()
+    pairs = lambda xs: [] if len(xs) == 0 else [(xs[0], xs[1])] + pairs(xs[2:])
+    res = line.leaveWhitespace().parseString(lineString)
+    metadata_type = res[0] # e.g. INFO/FORMAT
+    schema = dict(pairs(res[1:]))
+    types = {
+        'Integer' : int,
+        'Float' : float,
+        'String' : str
+    }
+    schema['Type'] = types[schema['Type']]
+    return metadata_type, schema
+    #return update_in(d['INFO'], ['Type'], compose(types.__getitem__, str.lower))
+    #res = line.leaveWhitespace().parseString(r'##INFO=<ID=technology.ILLUMINA,Number=A,Type=Float,Description="Fraction of observations supporting the alternate observed in reads from ILLUMINA">')
 
 '''
 reads_and_indices = st.integers(min_value=1,max_value=10).flatmap(
